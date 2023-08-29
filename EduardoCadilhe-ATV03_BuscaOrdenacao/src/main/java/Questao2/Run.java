4b2805393e1c5f9ec1f6aed05996ce989addb6eb
Eduardo Cadilhe <eduardo.00.1du@gmail.com>
2023-08-29 16:46:04 -0300

    Classe Run para Questao 2

---
+++ EduardoCadilhe-ATV03_BuscaOrdenacao/src/main/java/Questao2/Run.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Questao2;
+
+import Questao1.*;
+
+/**
+ *
+ * @author Cadilhe
+ */
+public class Run {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
